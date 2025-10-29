3e54172e4c501a41962df0b878457e41b9731f2b
Ruslan Melnyk <kanevgoga2@gmail.com>
2025-10-29 02:05:36 +0200

    sacas

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -22,6 +22,7 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
   const { slug } = await params
   const res = await sdk.GetPageBySlug({ slug })
   const page = res?.Pages?.docs?.[0]
+  console.log(page);
   if (!page) return null
 
 
